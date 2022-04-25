ecc1e74a26c2599e794d11bd5470a52409c49460
Ivan Jozic <ijozic@hotmail.com>
2022-04-25 13:45:31 +0200

    Added new services (company, owner, pipeline).

--- crm.go
+++ crm.go
@@ -9,13 +9,20 @@ const (
 )
 
 type CRM struct {
-	Contact ContactService
-	Deal    DealService
+	Company  CompanyService
+	Contact  ContactService
+	Deal     DealService
+	Owner    OwnerService
+	Pipeline PipelineService
 }
 
 func newCRM(c *Client) *CRM {
 	crmPath := fmt.Sprintf("%s/%s", crmBasePath, c.apiVersion)
 	return &CRM{
+		Company: &CompanyServiceOp{
+			companyPath: fmt.Sprintf("%s/%s/%s", crmPath, objectsBasePath, companyBasePath),
+			client:      c,
+		},
 		Contact: &ContactServiceOp{
 			contactPath: fmt.Sprintf("%s/%s/%s", crmPath, objectsBasePath, contactBasePath),
 			client:      c,
@@ -23,6 +30,14 @@ func newCRM(c *Client) *CRM {
 		Deal: &DealServiceOp{
 			dealPath: fmt.Sprintf("%s/%s/%s", crmPath, objectsBasePath, dealBasePath),
 			client:   c,
+		},
+		Owner: &OwnerServiceOp{
+			ownerPath: fmt.Sprintf("%s/%s", crmPath, ownerBasePath),
+			client:    c,
+		},
+		Pipeline: &PipelineServiceOp{
+			pipelinePath: fmt.Sprintf("%s/%s", crmPath, pipelineBasePath),
+			client:       c,
 		},
 	}
 }
